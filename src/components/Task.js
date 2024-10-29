81a8e48e01ede594ac53f05424909f65011f424b
David Velasquez <david.velasquez-c@mail.escuelaing.edu.co>
2024-10-28 19:16:48 -0500

    semi final changes front2

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,7 +7,7 @@ function Tasks() {
     const navigate = useNavigate();
     const [tasks, setTasks] = React.useState([]);
     const context = useOutletContext();
-    const { userData, token, setToken, roles } = context || {};
+    const { userData, roles } = context || {}; // Removed unused variables token and setToken
     console.log("Context data:", context); // Para verificar el contenido completo
 
     const handleLogout = async () => {
@@ -63,7 +63,7 @@ function Tasks() {
                     <li>Juan Sebastian Velasquez Rodriguez</li>
                     <li>Santiago Alberto Naranjo Abril</li>
                 </ul>
-                {userData.roles.includes("ROLE_ADMIN") && (
+                {roles.includes("ROLE_ADMIN") && (
                     <div>
                         <button className="container1" onClick={openAnalytics}>
                             Ver Analítica de Tareas
